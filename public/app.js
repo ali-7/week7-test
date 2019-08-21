7a8e2827baac1ba7723afe82449205f1b16908c7
ali <ali.7@outlook.com>
2019-08-21 16:53:24 +0300

    update-public-files

---
+++ public/app.js
@@ -0,0 +1,15 @@
+fetch("/cities")
+  .then(res => res.json())
+  .then(cities => {
+    const table = document.getElementById("cities-table");
+    cities.forEach(c => {
+      const row = document.createElement("tr");
+      const name = document.createElement("td");
+      name.textContent = c.name;
+      row.appendChild(name);
+      const city = document.createElement("td");
+      city.textContent = c.city;
+      row.appendChild(city);
+      table.appendChild(row);
+    });
+  });
