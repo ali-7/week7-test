791a37a529b066aba3e6a56c43865a30b56fb15b
Mohammed-Q96 <mhmmade@gmail.com>
2019-08-25 06:41:10 +0300

    fix display data bug

--- server/controllers/getCities.js
+++ server/controllers/getCities.js
@@ -3,7 +3,7 @@ const getCities = require('../database/queries/getCities');
 module.exports = (req, res, next) => {
   getCities()
     .then(result => {
-      return { cities: result.rows };
+      return result.rows;
     })
     .then(result => res.json(result))
     .catch(next);
