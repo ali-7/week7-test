a2af883ec9e3757f931168188731d5902d6d30f3
MaiUbeid <ubead95@gmail.com>
2019-08-30 20:47:05 +0300

    fix edit name

--- server/database/config/build.sql
+++ server/database/config/build.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS cities CASCADE;
 
 CREATE TABLE cities (
   id SERIAL PRIMARY KEY,
-  user_name VARCHAR(50),
+  name VARCHAR(50),
   city VARCHAR(70)
 );
 
-INSERT INTO cities (user_name, city) VALUES ('Mai', 'Gaza'),('Noor', 'Ramallah'),('Tala', 'Rafah');
+INSERT INTO cities (name, city) VALUES ('Mai', 'Gaza'),('Noor', 'Ramallah'),('Tala', 'Rafah');
 
 COMMIT;
