f3d96cee2a60447bcdf81484d14268fa5fd235ba
Hassan Al-najjar <ha77an19@gmail.com>
2020-03-03 21:35:18 +0200

    handel pages citys

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const { getData } = require("../database/queries/getCities");
+const { addData } = require("../database/queries/addCity");
 
 const router = express.Router();
-
+router.get("/cities", (req, res) => {
+  getData()
+    .then(result => res.json(result.rows))
+    .catch(e => console.log("error >> " + e));
+});
+router.post("/add-city", (req, res) => {
+  addData(req.body)
+    .then(() => res.redirect("/"))
+    .catch(e => console.log("error >> " + e));
+});
 
 module.exports = router;
